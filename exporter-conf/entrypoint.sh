dad02ab4eccacc78c97efc8b5a316c6cf0615684
louis guillou <louis.guillou@nxp.com>
2021-02-26 15:14:03 +0100

    commit

--- exporter-conf/entrypoint.sh
+++ exporter-conf/entrypoint.sh
@@ -2,4 +2,4 @@
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # ser2net -c ${SCRIPTPATH}/ser2net.conf
 # wait-for-it is a simple script that waits until the coordinator service is up before starting the exporter
-/opt/wait-for-it/wait-for-it.sh coordinator:20408 -- labgrid-exporter -n default -x ws://coordinator:20408/ws ${SCRIPTPATH}/config.yaml
+/opt/wait-for-it/wait-for-it.sh coordinator:20408 -- labgrid-exporter -n my_exporter -x ws://coordinator:20408/ws ${SCRIPTPATH}/config.yaml
